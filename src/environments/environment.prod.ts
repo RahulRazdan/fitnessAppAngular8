550ae7f29145233ed348d111c015bb703d233967
RahulRazdan <razdanrahul0@gmail.com>
2020-03-08 19:40:49 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyChqpAVOxhLLOSca3j3sADmvisGXUukk6Y",
+    authDomain: "ng-custom-fitness-tracker.firebaseapp.com",
+    databaseURL: "https://ng-custom-fitness-tracker.firebaseio.com",
+    projectId: "ng-custom-fitness-tracker",
+    storageBucket: "ng-custom-fitness-tracker.appspot.com",
+    messagingSenderId: "417348813982",
+    appId: "1:417348813982:web:df4f66611cf664367b678f"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyChqpAVOxhLLOSca3j3sADmvisGXUukk6Y",
+    authDomain: "ng-custom-fitness-tracker.firebaseapp.com",
+    databaseURL: "https://ng-custom-fitness-tracker.firebaseio.com",
+    projectId: "ng-custom-fitness-tracker",
+    storageBucket: "ng-custom-fitness-tracker.appspot.com",
+    messagingSenderId: "417348813982",
+    appId: "1:417348813982:web:df4f66611cf664367b678f"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
